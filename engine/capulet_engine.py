1486e3ad3f83015433d3910e2ca90eb7896e78f8
Monaem Tarek <11410120210220@stud.cu.edu.eg>
2023-02-09 20:57:44 +0200

    refactored code

--- engine/capulet_engine.py
+++ engine/capulet_engine.py
@@ -1,13 +1,12 @@
 from abc import ABC
+from Engine import Engine
 
-from car import Car
 
-
-class CapuletEngine(Car, ABC):
+class CapuletEngine(ABC, Engine):
     def __init__(self, last_service_date, current_mileage, last_service_mileage):
-        super().__init__(last_service_date)
+        super().__init__()
         self.current_mileage = current_mileage
         self.last_service_mileage = last_service_mileage
 
-    def engine_should_be_serviced(self):
+    def needs_service(self):
         return self.current_mileage - self.last_service_mileage > 30000
--- engine/sternman_engine.py
+++ engine/sternman_engine.py
@@ -1,14 +1,15 @@
 from abc import ABC
 
-from car import Car
+from engine import Engine
 
 
-class SternmanEngine(Car, ABC):
+class SternmanEngine(Engine, ABC):
     def __init__(self, last_service_date, warning_light_is_on):
-        super().__init__(last_service_date)
+        super().__init__()
         self.warning_light_is_on = warning_light_is_on
+        self.last_service_date = last_service_date
 
-    def engine_should_be_serviced(self):
+    def needs_service(self):
         if self.warning_light_is_on:
             return True
         else:
